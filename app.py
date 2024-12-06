8a94e43adeff3a426329961e8fc69471ae4bbfda
Johanes Nicky C. S <apanyaclay1@gmail.com>
2024-12-07 03:07:50 +0700

    Fix

--- app.py
+++ app.py
@@ -3,11 +3,15 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '-1'  # Menonaktifkan penggunaan GPU
 from flask import Flask, request, jsonify
 import tensorflow as tf
 import numpy as np
+import joblib
 
 # Inisialisasi Flask app
 app = Flask(__name__)
 # Load model TensorFlow (.h5)
-model = tf.keras.models.load_model('model1.h5', compile=False)
+model = tf.keras.models.load_model('model.h5', compile=False)
+
+scaler_X = joblib.load('scaler_X.pkl')  # Muat scaler untuk fitur input
+scaler_y = joblib.load('scaler_y.pkl')  # Muat scaler untuk target output
 
 # Route untuk endpoint prediksi
 @app.route('/predict', methods=['POST'])
@@ -25,11 +29,24 @@ def predict():
         
         # Dimensi Big Five Personality
         traits = ['Keterbukaan Sosial, Energi, dan Antusiasme', 'Kestabilan Emosi', 'Kesepakatan', 'Ketelitian', 'Keterbukaan terhadap Pengalaman']
+        
+        # Pastikan semua elemen adalah angka
+        try:
+            input_data = np.array(data['input'], dtype=float).reshape(1, -1)
+        except ValueError:
+            return jsonify({'error': 'All elements in the input list must be numerical values.'}), 400
+        
+        # Terapkan MinMaxScaler pada input data yang masuk
+        input_data_scaled = scaler_X.transform(input_data)
 
         # Lakukan prediksi
-        predictions = model.predict(input_data)
+        predictions = model.predict(input_data_scaled).flatten()
 
-        output = {traits[i]: float(predictions[i]) for i in range(5)}
+        # Inverse transformasi prediksi untuk mengembalikannya ke skala asli
+        predictions_original = scaler_y.inverse_transform(predictions.reshape(1, -1)).flatten()
+
+        # Format hasil prediksi
+        output = {traits[i]: float(predictions_original[i]) for i in range(5)}
         return jsonify({'prediction': output})
     except Exception as e:
         return jsonify({'error': str(e)})
